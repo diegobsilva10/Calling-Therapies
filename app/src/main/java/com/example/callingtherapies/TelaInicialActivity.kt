0c2de846ccaacbe391850384f223e3b6bba13e99
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-17 22:15:40 -0300

    Activity Tela inicial criada

--- app/src/main/java/com/example/callingtherapies/TelaInicialActivity.kt
+++ app/src/main/java/com/example/callingtherapies/TelaInicialActivity.kt
@@ -4,22 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.SearchView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
+import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class TelaInicialActivity : DebugActivity2() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.testandolayout)
+        setContentView(R.layout.tela_inicial2)
 
         val args = intent.extras
         val nome_usuario2 = args?.getString("nome_usuario")
         Toast.makeText(this, "Bem Vindo: $nome_usuario2", Toast.LENGTH_SHORT).show()
         val nome_usuario = args?.getString("nome_usuario")
         mensagemTelaInicial.text = nome_usuario
-
         //IMPLEMENTA A TOOLBAR CRIADA
         setSupportActionBar(toolbar)
         // alterar título da ActionBar
@@ -28,6 +29,51 @@ class TelaInicialActivity : DebugActivity2() {
         // up navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        //EVENTOS DE CLICK ATRAVÉS DO ID DO BOTÃO LOGIN (ID LOCALIZADO tela_inicial2.xml)
+        //AÇÃO DE BOTÃO DO PRODUTOS
+        val buttonProduto: Button = findViewById(R.id.botao1)
+        buttonProduto.setOnClickListener {
+
+//            //EXIBE MENSAGEM NA TELA
+//            Toast.makeText(this,"Produtos",Toast.LENGTH_SHORT).show()
+
+
+            //INTENT CRIADA PARA MUDAR PARA A ACTIVITY PRODUTO AO CLICAR NO BOTÃO
+            var intent = Intent(this, ActivityProduto::class.java)
+            intent.putExtra("nome_usuario", nome_usuario)
+            startActivity(intent)
+        }
+
+        //AÇÃO DE BOTÃO DO TRATAMENTO
+        val buttonTratamentos: Button = findViewById(R.id.botao2)
+        buttonTratamentos.setOnClickListener {
+
+//            //EXIBE MENSAGEM NA TELA
+//            Toast.makeText(this,"Produtos",Toast.LENGTH_SHORT).show()
+
+
+            //INTENT CRIADA PARA MUDAR PARA A ACTIVITY PRODUTO AO CLICAR NO BOTÃO
+            var intent = Intent(this, ActivityTratamentos::class.java)
+            intent.putExtra("nome_usuario", nome_usuario)
+            startActivity(intent)
+
+
+        }
+        //AÇÃO DE BOTÃO DE AGENDA
+        val buttonAgenda: Button = findViewById(R.id.botao2)
+        buttonAgenda.setOnClickListener {
+
+//            //EXIBE MENSAGEM NA TELA
+//            Toast.makeText(this,"Produtos",Toast.LENGTH_SHORT).show()
+
+
+            //INTENT CRIADA PARA MUDAR PARA A ACTIVITY PRODUTO AO CLICAR NO BOTÃO
+            var intent = Intent(this, ActivityTratamentos::class.java)
+            intent.putExtra("nome_usuario", nome_usuario)
+            startActivity(intent)
+
+
+        }
     }
 
     // método sobrescrito para inflar o menu na Actionbar
