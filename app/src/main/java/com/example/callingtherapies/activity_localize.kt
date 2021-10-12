fa8454a5275969d1ad8f3349de98db60d58513b2
diegobsailva10 <diegobdasilva12@gmail.com>
2021-10-12 13:48:03 -0300

    implementação da activity localize

---
+++ app/src/main/java/com/example/callingtherapies/activity_localize.kt
@@ -0,0 +1,11 @@
+package com.example.callingtherapies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_localize : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_localize)
+    }
+}
